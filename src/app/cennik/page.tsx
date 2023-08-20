23f7f4e878bebb2fe89452249e73f9ca7e59c351
Wojciech Lamperski <wolamp20@gmail.com>
2023-08-20 15:37:12 +0200

    wip: added pricing

---
+++ src/app/cennik/page.tsx
@@ -0,0 +1,140 @@
+
+
+export default function Pricing() {
+    return (
+        <section className="pricing main__section main__section--margins">
+             <div className="pricing__wrapper">
+             <table className="table pricing__table pricing__table--margin-mobile pricing__table--margin-bottom">
+                <caption className="table__caption">KONSULTACJE MEDYCZNE</caption>
+                <tbody className="table__body">
+                    <tr className="table__row">
+                    <td className="table__cell table__cell--name" data-label="Name">Lekarz specjalista balneologii i medycyny fizykalnej</td>
+                    <td className="table__cell table__cell--price" data-label="Price"> 40 zł</td>
+                    </tr>
+                </tbody>
+                </table>
+                <table className="table pricing__table pricing__table--margin-bottom">
+                <caption className="table__caption">KINEZYTERAPIA</caption>
+                <tbody className="table__body">
+                    <tr className="table__row">
+                    <td className="table__cell table__cell--name" data-label="Name">Gimnastyka grupowa </td>
+                    <td className="table__cell table__cell--price" data-label="Price">10 zł</td>
+                    </tr>
+                    <tr className="table__row">
+                    <td className="table__cell table__cell--name" data-label="Name">Gimnastyka indywidualna </td>
+                    <td className="table__cell table__cell--price" data-label="Price">20 zł</td>
+                    </tr>
+                    <tr className="table__row">
+                    <td className="table__cell table__cell--name" data-label="Name">Instruktaż do ćwiczeń indywidualnych</td>
+                    <td className="table__cell table__cell--price" data-label="Price">20 zł</td>
+                    </tr>
+                    <tr className="table__row">
+                    <td className="table__cell table__cell--name" data-label="Name">Ćwiczenia w odciążeniu - 1 staw biodrowy / 2 stawy </td>
+                    <td className="table__cell table__cell--price" data-label="Price">12 / 18 zł</td>
+                    </tr>
+                    <tr className="table__row">
+                    <td className="table__cell table__cell--name" data-label="Name">Ćwiczenia w odciążeniu stawów barkowych</td>
+                    <td className="table__cell table__cell--price" data-label="Price">12 zł</td>
+                    </tr>
+                    <tr className="table__row">
+                    <td className="table__cell table__cell--name" data-label="Name">Ćwiczenia czynne</td>
+                    <td className="table__cell table__cell--price" data-label="Price">12 zł</td>
+                    </tr>
+                    <tr className="table__row">
+                    <td className="table__cell table__cell--name" data-label="Name">Ćwiczenia na przyrządach (rower, atlas itp.) </td>
+                    <td className="table__cell table__cell--price" data-label="Price">12 zł</td>
+                    </tr>
+                    <tr className="table__row">
+                    <td className="table__cell table__cell--name" data-label="Name">Ćwiczenia przy stołach manualnych</td>
+                    <td className="table__cell table__cell--price" data-label="Price">12 zł</td>
+                    </tr>
+                </tbody>
+                </table>
+                <table className="table pricing__table ">
+                <caption className="table__caption">
+                    FIZYKOTERAPIA
+                </caption>
+                <tbody className="table__body">
+                    <tr className="table__row">
+                    <td className="table__cell table__cell--name" data-label="Name">Diadynamik </td>
+                    <td className="table__cell table__cell--price" data-label="Price"> 11 zł</td>
+                    </tr>
+                    <tr className="table__row">
+                    <td className="table__cell table__cell--name" data-label="Name">Sollux </td>
+                    <td className="table__cell table__cell--price" data-label="Price"> 10 zł</td>
+                    </tr>
+                    <tr className="table__row">
+                    <td className="table__cell table__cell--name" data-label="Name">Galwanizacja </td>
+                    <td className="table__cell table__cell--price" data-label="Price"> 11 zł</td>
+                    </tr>
+                    <tr className="table__row">
+                    <td className="table__cell table__cell--name" data-label="Name">Jonoforeza </td>
+                    <td className="table__cell table__cell--price" data-label="Price">11 zł</td>
+                    </tr>
+                    <tr className="table__row">
+                    <td className="table__cell table__cell--name" data-label="Name">Interdyn </td>
+                    <td className="table__cell table__cell--price" data-label="Price"> 11 zł</td>
+                    </tr>
+                    <tr className="table__row">
+                    <td className="table__cell table__cell--name" data-label="Name">Magnetoterapia </td>
+                    <td className="table__cell table__cell--price" data-label="Price"> 11 zł</td>
+                    </tr>
+                    <tr className="table__row">
+                    <td className="table__cell table__cell--name" data-label="Name">Laser (1 punkt) </td>
+                    <td className="table__cell table__cell--price" data-label="Price"> 11 zł</td>
+                    </tr>
+                    <tr className="table__row">
+                    <td className="table__cell table__cell--name" data-label="Name">Terapuls </td>
+                    <td className="table__cell table__cell--price" data-label="Price"> 11 zł</td>
+                    </tr>
+                    <tr className="table__row">
+                    <td className="table__cell table__cell--name" data-label="Name">Ultradźwięki </td>
+                    <td className="table__cell table__cell--price" data-label="Price"> 12 zł</td>
+                    </tr>
+                    <tr className="table__row">
+                    <td className="table__cell table__cell--name" data-label="Name">Tens </td>
+                    <td className="table__cell table__cell--price" data-label="Price">11 zł</td>
+                    </tr>
+                    <tr className="table__row">
+                    <td className="table__cell table__cell--name" data-label="Name">Krioterapia (do 3 min / 1 okolica) </td>
+                    <td className="table__cell table__cell--price" data-label="Price"> 12 zł</td>
+                    </tr>
+                    <tr className="table__row">
+                    <td className="table__cell table__cell--name" data-label="Name">Thermo </td>
+                    <td className="table__cell table__cell--price" data-label="Price"> 13 zł</td>
+                    </tr>
+                    <tr className="table__row">
+                    <td className="table__cell table__cell--name" data-label="Name">Masaż wirowy L / S </td>
+                    <td className="table__cell table__cell--price" data-label="Price"> 13 zł</td>
+                    </tr>
+                </tbody>
+                </table>
+                <table className="table pricing__table ">
+                <caption className="table__caption">MASAŻE</caption>
+                <tbody className="table__body">
+                     <tr className="table__row">
+                    <td className="table__cell table__cell--name" data-label="Name">Masaż suchy częściowy (1 okolica) </td>
+                    <td className="table__cell table__cell--price" data-label="Price"> 25 zł </td>
+                    </tr>
+                    <tr className="table__row">
+                    <td className="table__cell table__cell--name" data-label="Name">Masaż suchy kręgosłupa (th-1 / s) </td>
+                    <td className="table__cell table__cell--price" data-label="Price"> 30 zł</td>
+                    </tr>
+                    <tr className="table__row">
+                    <td className="table__cell table__cell--name" data-label="Name">Masaż suchy (C,Th, L / S) </td>
+                    <td className="table__cell table__cell--price" data-label="Price"> 40 zł</td>
+                    </tr>
+                    <tr className="table__row">
+                    <td className="table__cell table__cell--name" data-label="Name">Masaż limfatyczny (1 okolica) </td>
+                    <td className="table__cell table__cell--price" data-label="Price"> 30 zł</td>
+                    </tr>
+                    <tr className="table__row">
+                    <td className="table__cell table__cell--name" data-label="Name">Masaż mechaniczny (BOA) </td>
+                    <td className="table__cell table__cell--price" data-label="Price">10 zł</td> 
+                    </tr>
+                </tbody>
+                </table>
+            </div>
+        </section>
+    )
+}
