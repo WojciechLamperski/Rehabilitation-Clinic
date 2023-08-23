eea9fa9cab7c15533d73a89a403027752ef94f94
Wojciech Lamperski <wolamp20@gmail.com>
2023-08-23 13:24:16 +0200

    wip: foldable menu closes when clicking on link element

--- src/components/Header/menuWithHamburger.tsx
+++ src/components/Header/menuWithHamburger.tsx
@@ -11,12 +11,12 @@ export default function MenuWithHamburger(){
         <>
               <nav className={toggle===true ? "header__menu menu menu--active" : "header__menu menu"} id="menu">
                 <ul className="menu__list">
-                 <PageTitle siteName="Zabiegi" />
-                 <PageTitle siteName="Cennik" />
-                 <PageTitle siteName="Rehabilitacje" />
-                 <PageTitle siteName="Nasz Zespół" url="zespol"  />
-                 <PageTitle siteName="Galeria" />
-                 <PageTitle siteName="Kontakt" />
+                 <PageTitle setToggle={setToggle} toggle={toggle} siteName="Zabiegi" />
+                 <PageTitle setToggle={setToggle} toggle={toggle} siteName="Cennik" />
+                 <PageTitle setToggle={setToggle} toggle={toggle} siteName="Rehabilitacje" />
+                 <PageTitle setToggle={setToggle} toggle={toggle} siteName="Nasz Zespół" url="zespol"  />
+                 <PageTitle setToggle={setToggle} toggle={toggle} siteName="Galeria" />
+                 <PageTitle setToggle={setToggle} toggle={toggle} siteName="Kontakt" />
                 </ul>
               </nav>
       
--- src/components/Header/pageTitle.tsx
+++ src/components/Header/pageTitle.tsx
@@ -2,14 +2,14 @@
 import { usePathname } from 'next/navigation'
 import Link from "next/link"
 
-export default function PageTitle({siteName, url}){
+export default function PageTitle({siteName, url, toggle, setToggle}){
 
     const pathname = usePathname();
     const title = `${siteName.charAt(0).toUpperCase() + siteName.slice(1)}`;
        
 
     return(
-        <li className={pathname===(url ? url : `/${siteName.toLowerCase().replace(/\s/g, '')}`) ? "menu__item menu__item--active" : "menu__item"}>
+        <li onClick={()=>{toggle===true && setToggle(false)}} className={pathname===(url ? url : `/${siteName.toLowerCase().replace(/\s/g, '')}`) ? "menu__item menu__item--active" : "menu__item"}>
             <Link href={url ? url : `/${siteName.toLowerCase().replace(/\s/g, '')}`} className="menu__link">
                 {title}
             </Link>
