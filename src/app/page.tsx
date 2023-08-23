9a08b792c1e04d255542933b2b6d72e22ca294cb
Wojciech Lamperski <wolamp20@gmail.com>
2023-08-23 09:05:18 +0200

    wip: can change slides

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -72,13 +72,13 @@ export default function Home() {
       </div>
       </div>
 
-      <button onClick={()=>{}} className="carousel__nav carousel__nav--right" data-carousel-button="next">
+      <button onClick={()=>{index===2 ? setIndex(0) : setIndex(index+1)}} className="carousel__nav carousel__nav--right" data-carousel-button="next">
       <svg className="carousel__svg" xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 96 960 960" width="22.8">
           <path d="m304 974-56-57 343-343-343-343 56-57 400 400-400 400Z" />
       </svg>
 
       </button>
-      <button onClick={()=>{}} className="carousel__nav carousel__nav--left" data-carousel-button="prev">
+      <button onClick={()=>{index===0 ? setIndex(2) : setIndex(index-1)}} className="carousel__nav carousel__nav--left" data-carousel-button="prev">
       <svg className="carousel__svg carousel__svg--left" xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 96 960 960" width="22.8">
           <path d="m304 974-56-57 343-343-343-343 56-57 400 400-400 400Z" />
       </svg>
