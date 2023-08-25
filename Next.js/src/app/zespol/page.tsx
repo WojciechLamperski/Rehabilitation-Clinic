c701c3de4c402b8afff5faa98631909215a08887
Wojciech Lamperski <wolamp20@gmail.com>
2023-08-25 15:51:36 +0200

    wip

--- Next.js/src/app/zespol/page.tsx
+++ Next.js/src/app/zespol/page.tsx
@@ -1,14 +1,34 @@
-export default function Team(){
+export default async function Team(){
+
+    const res = await fetch("http://172.28.64.1:1337/api/teams");
+    const json = await res.json();
+    const team = json.data;
+    console.log(team)
+
     return(
         <section className="team main__section main__section--margins">
             <div className="team__wrapper">
-                <div className="team__card card">
-                <img className="team__image" src="img/team/team_01.jpg" alt="" />
-                <h3 className="team__name">
-                    Lorem Ipsum
-                </h3>
-                <p className="team__degree">Lek. med. spec. fizjoterapii i balneoklimatologii</p>
+
+                {team.map((member)=>{
+                    return(
+                        <div className="team__card card">
+                            <img className="team__image" src={member.attributes.imageURL} alt="" />
+                            <h3 className="team__name">
+                                {member.attributes.name}
+                            </h3>
+                            <p className="team__degree">{member.attributes.title}</p>
+                        </div>
+                    )
+                })}
+                
+                {/* <div className="team__card card">
+                    <img className="team__image" src="img/team/team_01.jpg" alt="" />
+                    <h3 className="team__name">
+                        Lorem Ipsum
+                    </h3>
+                    <p className="team__degree">Lek. med. spec. fizjoterapii i balneoklimatologii</p>
                 </div>
+
                 <div className="team__card card">
                 <img className="team__image" src="img/team/team_02.jpg" alt="" />
                 <h3 className="team__name">
@@ -106,7 +126,7 @@ export default function Team(){
                     Lorem Ipsum
                 </h3>
                 <p className="team__degree">Mgr fizjoterapii</p>
-                </div>
+                </div> */}
             </div>
 
         </section>
