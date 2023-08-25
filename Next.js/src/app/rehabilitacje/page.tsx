d6f1f18e6f1c7c9fbaf0592b076b5a00ba19c2e8
Wojciech Lamperski <wolamp20@gmail.com>
2023-08-25 15:11:46 +0200

    wip

--- Next.js/src/app/rehabilitacje/page.tsx
+++ Next.js/src/app/rehabilitacje/page.tsx
@@ -2,9 +2,13 @@ import ReactMarkdown from "react-markdown";
 
 export default async function Rehab(){
 
-    const res = await fetch("http://172.28.64.1:1337/api/rehabs");
-    const json = await res.json();
-    const rehabs = json.data;
+    const resRehabs = await fetch("http://172.28.64.1:1337/api/rehabs");
+    const jsonRehabs = await resRehabs.json();
+    const rehabs = jsonRehabs.data;
+
+    const resFunding = await fetch("http://172.28.64.1:1337/api/funding");
+    const jsonFunding = await resFunding.json();
+    const funding = jsonFunding.data;
 
     return(
         <>
@@ -38,39 +42,7 @@ export default async function Rehab(){
                 <div className="card__container">
                     <div className="card">
                     <p className="card__desc">
-                        Zgodnie z Rozporządzeniem Ministra Zdrowia w sprawie świadczeń
-                        gwarantowanych z zakresu rehabilitacji leczniczej (Dz. U. z 2018 r.
-                        poz. 465 z późn.zm [1].), zabiegi fizjoterapeutyczne w warunkach
-                        domowych są udzielane świadczeniobiorcom z zaburzeniami funkcji
-                        motorycznych spowodowanymi:
-
-                        ogniskowymi uszkodzeniami mózgu (stanami po zatorach mózgowych,
-                        udarach krwotocznych mózgu, urazach) - przez okres do 12 miesięcy od
-                        dnia powstania ogniskowego uszkodzenia mózgu;
-                        ciężkimi uszkodzeniami centralnego i obwodowego układu nerwowego
-                        odpowiadającymi 5. stopniowi skali oceny stopnia inwalidztwa (warunku
-                        tego nie stosuje się do dzieci do ukończenia 18. roku życia);
-                        uszkodzeniem rdzenia kręgowego - przez okres 12 miesięcy od dnia
-                        powstania uszkodzenia rdzenia kręgowego;
-                        chorobami przewlekle postępującymi, w szczególności: miopatiami,
-                        chorobą Parkinsona, zapaleniem wielomięśniowym, rdzeniowym zanikiem
-                        mięśni, guzami mózgu, procesami demielinizacyjnymi, kolagenozami,
-                        przewlekłymi zespołami pozapiramidowymi, reumatoidalnym zapaleniem
-                        stawów;
-                        chorobami zwyrodnieniowymi stawów biodrowych lub kolanowych, po
-                        zabiegach endoprotezoplastyki stawu - przez okres 6 miesięcy od dnia
-                        wykonania operacji;
-                        urazami kończyn dolnych - przez okres 6 miesięcy od dnia powstania
-                        urazu;
-                        osobom w stanie wegetatywnym lub apalicznym.
-                        Skierowanie na zabiegi fizjoterapii w warunkach domowych wystawia
-                        lekarz podstawowej opieki zdrowotnej (POZ) lub inny lekarz
-                        ubezpieczenia zdrowotnego.
-
-                        Skierowanie na zabiegi fizjoterapeutyczne w warunkach domowych jest
-                        ważne 30 dni od daty wystawienia. W tym czasie skierowanie musi
-                        zostać zarejestrowane, bez względu na to, kiedy rozpocznie się
-                        rehabilitacja.
+                       {funding.attributes.body}
                     </p>
                     </div>
                 </div>
