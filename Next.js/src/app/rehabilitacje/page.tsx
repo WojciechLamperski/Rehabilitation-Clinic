f81186f29a4c57fc26cf8df155de6254872ed2d3
Wojciech Lamperski <wolamp20@gmail.com>
2023-08-25 15:04:32 +0200

    wip

--- Next.js/src/app/rehabilitacje/page.tsx
+++ Next.js/src/app/rehabilitacje/page.tsx
@@ -1,4 +1,11 @@
-export default function Rehab(){
+import ReactMarkdown from "react-markdown";
+
+export default async function Rehab(){
+
+    const res = await fetch("http://172.28.64.1:1337/api/rehabs");
+    const json = await res.json();
+    const rehabs = json.data;
+
     return(
         <>
             <h1 className="heading">Oferujemy dwa rodzaje rehabilitacji</h1>
@@ -9,19 +16,19 @@ export default function Rehab(){
                         <h1 className="rehab__title">
                             Rehabilitacja w warunkach domowych
                         </h1>
-                        <p className="card__desc">
-                            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur.
-                        </p>
+                        <ReactMarkdown className="card__desc">
+                            {rehabs[0].attributes.body} 
+                        </ReactMarkdown>
                     </div>
                 </div>
             </section>
             <section className="main__section main__section--margins">
                 <div className="card__container">
                     <div className="card">
-                    <h1 className="rehab__title">Rehabilitacja ambulatoryjna</h1>
-                    <p className="card__desc">
-                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur.
-                    </p>
+                        <h1 className="rehab__title">Rehabilitacja ambulatoryjna</h1>
+                        <ReactMarkdown className="card__desc">
+                            {rehabs[1].attributes.body}  
+                        </ReactMarkdown>
                     </div>
                 </div>
             </section>
