be80532c812d86084293df153c0d51adf98e54d3
Wojciech Lamperski <wolamp20@gmail.com>
2023-09-09 13:39:35 +0200

    wip: added new team images

--- src/app/zespol/page.tsx
+++ src/app/zespol/page.tsx
@@ -3,109 +3,123 @@ export default function Team(){
         <section className="team main__section main__section--margins">
             <div className="team__wrapper">
                 <div className="team__card card">
-                <img className="team__image" src="img/team/team_01.jpg" alt="" />
-                <h3 className="team__name">
-                    Lorem Ipsum
-                </h3>
-                <p className="team__degree">Lek. med. spec. fizjoterapii i balneoklimatologii</p>
+                    <img className="team__image" src="img/team/Irena Waligorska 1.jpg" alt="" />
+                    <h3 className="team__name">
+                        Dr Irena Waligorska
+                    </h3>
+                    <p className="team__degree">Lek. med. spec. fizjoterapii i balneoklimatologii</p>
                 </div>
                 <div className="team__card card">
-                <img className="team__image" src="img/team/team_02.jpg" alt="" />
-                <h3 className="team__name">
-                    Lorem Ipsum
-                </h3>
-                <p className="team__degree">Mgr rehabilitacji ruchowej</p>
+                    <img className="team__image" src="img/team/Katarzyna Rusiecka 2.jpg" alt="" />
+                    <h3 className="team__name">
+                        Lorem Ipsum
+                    </h3>
+                    <p className="team__degree">Fizjoterapeuta</p>
                 </div>
                 <div className="team__card card">
-                <img className="team__image" src="img/team/team_05.jpg" alt="" />
-                <h3 className="team__name">
-                    Lorem Ipsum
-                </h3>
-                <p className="team__degree">Mgr rehabilitacji ruchowej</p>
+                    <img className="team__image" src="img/team/Przemysław Duda 3.jpg" alt="" />
+                    <h3 className="team__name">
+                        Lorem Ipsum
+                    </h3>
+                    <p className="team__degree">Fizjoterapeuta</p>
                 </div>
                 <div className="team__card card">
-                <img className="team__image" src="img/team/team_04.jpg" alt="" />
-                <h3 className="team__name">
-                    Lorem Ipsum
-                </h3>
-                <p className="team__degree">Mgr fizjoterapii</p>
+                    <img className="team__image" src="img/team/Idalia Jarosz 4.jpg" alt="" />
+                    <h3 className="team__name">
+                        Lorem Ipsum
+                    </h3>
+                    <p className="team__degree">Mgr fizjoterapii</p>
                 </div>
                 <div className="team__card card">
-                <img className="team__image" src="img/team/team_01.jpg" alt="" />
-                <h3 className="team__name">
-                    Lorem Ipsum
-                </h3>
-                <p className="team__degree">Lek. med. spec. fizjoterapii i balneoklimatologii</p>
+                    <img className="team__image" src="img/team/Daria Mazgoła 5.jpg" alt="" />
+                    <h3 className="team__name">
+                        Lorem Ipsum
+                    </h3>
+                    <p className="team__degree">Lek. med. spec. fizjoterapii i balneoklimatologii</p>
                 </div>
                 <div className="team__card card">
-                <img className="team__image" src="img/team/team_02.jpg" alt="" />
-                <h3 className="team__name">
-                    Lorem Ipsum
-                </h3>
-                <p className="team__degree">Mgr rehabilitacji ruchowej</p>
+                    <img className="team__image" src="img/team/Agnieszka Grzegory 6.jpg" alt="" />
+                    <h3 className="team__name">
+                        Lorem Ipsum
+                    </h3>
+                    <p className="team__degree">Fizjoterapeuta</p>
                 </div>
                 <div className="team__card card">
-                <img className="team__image" src="img/team/team_03.jpg" alt="" />
-                <h3 className="team__name">
-                    Lorem Ipsum
-                </h3>
-                <p className="team__degree">Mgr rehabilitacji ruchowej</p>
+                    <img className="team__image" src="img/team/Roger Nowacki 7.jpg" alt="" />
+                    <h3 className="team__name">
+                        Lorem Ipsum
+                    </h3>
+                    <p className="team__degree">Fizjoterapeuta</p>
                 </div>
                 <div className="team__card card">
-                <img className="team__image" src="img/team/team_04.jpg" alt="" />
-                <h3 className="team__name">
-                    Lorem Ipsum
-                </h3>
-                <p className="team__degree">Mgr fizjoterapii</p>
+                    <img className="team__image" src="img/team/Oksana Kaniuk 8.jpg" alt="" />
+                    <h3 className="team__name">
+                        Lorem Ipsum
+                    </h3>
+                    <p className="team__degree">Mgr fizjoterapii</p>
                 </div>
                 <div className="team__card card">
-                <img className="team__image" src="img/team/team_01.jpg" alt="" />
-                <h3 className="team__name">
-                    Lorem Ipsum
-                </h3>
-                <p className="team__degree">Lek. med. spec. fizjoterapii i balneoklimatologii</p>
+                    <img className="team__image" src="img/team/Ewa Bajraszewska.jpg" alt="" />
+                    <h3 className="team__name">
+                        Lorem Ipsum
+                    </h3>
+                    <p className="team__degree">Lek. med. spec. fizjoterapii i balneoklimatologii</p>
                 </div>
                 <div className="team__card card">
-                <img className="team__image" src="img/team/team_02.jpg" alt="" />
-                <h3 className="team__name">
-                    Lorem Ipsum
-                </h3>
-                <p className="team__degree">Mgr rehabilitacji ruchowej</p>
+                    <img className="team__image" src="img/team/Joanna Zakrzewska.jpg" alt="" />
+                    <h3 className="team__name">
+                        Lorem Ipsum
+                    </h3>
+                    <p className="team__degree">Fizjoterapeuta</p>
                 </div>
                 <div className="team__card card">
-                <img className="team__image" src="img/team/team_03.jpg" alt="" />
-                <h3 className="team__name">
-                    Lorem Ipsum
-                </h3>
-                <p className="team__degree">Mgr rehabilitacji ruchowej</p>
+                    <img className="team__image" src="img/team/Małgorzata Łężna.jpg" alt="" />
+                    <h3 className="team__name">
+                        Lorem Ipsum
+                    </h3>
+                    <p className="team__degree">Fizjoterapeuta</p>
                 </div>
                 <div className="team__card card">
-                <img className="team__image" src="img/team/team_04.jpg" alt="" />
-                <h3 className="team__name">
-                    Lorem Ipsum
-                </h3>
-                <p className="team__degree">Mgr fizjoterapii</p>
+                    <img className="team__image" src="img/team/Marek Stasiak.jpg" alt="" />
+                    <h3 className="team__name">
+                        Lorem Ipsum
+                    </h3>
+                    <p className="team__degree">Mgr fizjoterapii</p>
                 </div>
                 <div className="team__card card">
-                <img className="team__image" src="img/team/team_01.jpg" alt="" />
-                <h3 className="team__name">
-                    Lorem Ipsum
-                </h3>
-                <p className="team__degree">Lek. med. spec. fizjoterapii i balneoklimatologii</p>
+                    <img className="team__image" src="img/team/Mariola Bejnarowicz.jpg" alt="" />
+                    <h3 className="team__name">
+                        Lorem Ipsum
+                    </h3>
+                    <p className="team__degree">Lek. med. spec. fizjoterapii i balneoklimatologii</p>
                 </div>
                 <div className="team__card card">
-                <img className="team__image" src="img/team/team_02.jpg" alt="" />
-                <h3 className="team__name">
-                    Lorem Ipsum
-                </h3>
-                <p className="team__degree">Mgr rehabilitacji ruchowej</p>
+                    <img className="team__image" src="img/team/Mariola Celmer.jpg" alt="" />
+                    <h3 className="team__name">
+                        Lorem Ipsum
+                    </h3>
+                    <p className="team__degree">Fizjoterapeuta</p>
                 </div>
                 <div className="team__card card">
-                <img className="team__image" src="img/team/team_04.jpg" alt="" />
-                <h3 className="team__name">
-                    Lorem Ipsum
-                </h3>
-                <p className="team__degree">Mgr fizjoterapii</p>
+                    <img className="team__image" src="img/team/Patrycja Kawałek.jpg" alt="" />
+                    <h3 className="team__name">
+                        Lorem Ipsum
+                    </h3>
+                    <p className="team__degree">Mgr fizjoterapii</p>
+                </div>
+                <div className="team__card card">
+                    <img className="team__image" src="img/team/empty.jpg" alt="" />
+                    <h3 className="team__name">
+                    Karolina Świderska-Karlikowska
+                    </h3>
+                    <p className="team__degree">Mgr fizjoterapii</p>
+                </div>
+                <div className="team__card card">
+                    <img className="team__image" src="img/team/empty.jpg" alt="" />
+                    <h3 className="team__name">
+                        Danuta Kamińska 
+                    </h3>
+                    <p className="team__degree">Masażysta</p>
                 </div>
             </div>
 
