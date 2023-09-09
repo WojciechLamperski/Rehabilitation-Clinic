e3da8ffbfe6d966c2445d6bfb196c45e7b85cb39
Wojciech Lamperski <wolamp20@gmail.com>
2023-09-09 12:19:41 +0200

    wip: changed style of table cells

--- src/app/rehabilitacje/page.tsx
+++ src/app/rehabilitacje/page.tsx
@@ -7,6 +7,7 @@ export default function Rehab(){
             <div className="card__container">
                     <div className="card">
                         <h1 className="rehab__title">
+                            <svg className="rehab__svg" xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48"><path d="M220-180h150v-250h220v250h150v-390L480-765 220-570v390Zm-60 60v-480l320-240 320 240v480H530v-250H430v250H160Zm320-353Z"/></svg>
                             Rehabilitacja w warunkach domowych
                         </h1>
                         <p className="card__desc">
@@ -18,7 +19,10 @@ export default function Rehab(){
             <section className="main__section main__section--margins">
                 <div className="card__container">
                     <div className="card">
-                    <h1 className="rehab__title">Rehabilitacja ambulatoryjna</h1>
+                    <h1 className="rehab__title">
+                            <svg className="rehab__svg" xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48"><path d="M426-136v-250L210-261l-55-93 217-126-216-125 54-93 216 125v-251h108v251l216-125 54 93-216 125 217 126-55 93-216-125v250H426Z"/></svg>
+                            Rehabilitacja ambulatoryjna
+                        </h1>
                     <p className="card__desc">
                         Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur.
                     </p>
