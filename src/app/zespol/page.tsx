4c953c941bb3a1a5bb804b25dcc4494a079987fc
Wojciech Lamperski <wolamp20@gmail.com>
2023-09-21 22:50:47 +0200

    wip: build with output

--- src/app/zespol/page.tsx
+++ src/app/zespol/page.tsx
@@ -14,112 +14,144 @@ export default function Team(){
                     <p className="team__degree">Lek. med. spec. fizjoterapii i balneoklimatologii</p>
                 </div>
                 <div className="team__card card">
-                    <img className="team__image" src="img/team/Katarzyna Rusiecka 2.jpg" alt="" />
+                    <div className="team__image team__image--wrapper">
+                        <Image objectFit='contain' className="team__image" fill src="img/team/Katarzyna Rusiecka 2.jpg" alt="" />
+                    </div>
                     <h3 className="team__name">
                         Lorem Ipsum
                     </h3>
                     <p className="team__degree">Fizjoterapeuta</p>
                 </div>
                 <div className="team__card card">
-                    <img className="team__image" src="img/team/Przemysław Duda 3.jpg" alt="" />
+                    <div className="team__image team__image--wrapper">
+                        <Image objectFit='contain' className="team__image" fill src="img/team/Przemysław Duda 3.jpg" alt="" />
+                    </div>
                     <h3 className="team__name">
                         Lorem Ipsum
                     </h3>
                     <p className="team__degree">Fizjoterapeuta</p>
                 </div>
                 <div className="team__card card">
-                    <img className="team__image" src="img/team/Idalia Jarosz 4.jpg" alt="" />
+                    <div className="team__image team__image--wrapper">
+                        <Image objectFit='contain' className="team__image" fill src="img/team/Idalia Jarosz 4.jpg" alt="" />
+                    </div>
                     <h3 className="team__name">
                         Lorem Ipsum
                     </h3>
                     <p className="team__degree">Mgr fizjoterapii</p>
                 </div>
                 <div className="team__card card">
-                    <img className="team__image" src="img/team/Daria Mazgoła 5.jpg" alt="" />
+                    <div className="team__image team__image--wrapper">
+                        <Image objectFit='contain' className="team__image" fill src="img/team/Daria Mazgoła 5.jpg" alt="" />
+                    </div>
                     <h3 className="team__name">
                         Lorem Ipsum
                     </h3>
                     <p className="team__degree">Lek. med. spec. fizjoterapii i balneoklimatologii</p>
                 </div>
                 <div className="team__card card">
-                    <img className="team__image" src="img/team/Agnieszka Grzegory 6.jpg" alt="" />
+                    <div className="team__image team__image--wrapper">
+                        <Image objectFit='contain' className="team__image" fill src="img/team/Agnieszka Grzegory 6.jpg" alt="" />
+                    </div>
                     <h3 className="team__name">
                         Lorem Ipsum
                     </h3>
                     <p className="team__degree">Fizjoterapeuta</p>
                 </div>
                 <div className="team__card card">
-                    <img className="team__image" src="img/team/Roger Nowacki 7.jpg" alt="" />
+                    <div className="team__image team__image--wrapper">
+                        <Image objectFit='contain' className="team__image" fill src="img/team/Roger Nowacki 7.jpg" alt="" />
+                    </div>
                     <h3 className="team__name">
                         Lorem Ipsum
                     </h3>
                     <p className="team__degree">Fizjoterapeuta</p>
                 </div>
                 <div className="team__card card">
-                    <img className="team__image" src="img/team/Oksana Kaniuk 8.jpg" alt="" />
+                    <div className="team__image team__image--wrapper">
+                        <Image objectFit='contain' className="team__image" fill src="img/team/Oksana Kaniuk 8.jpg" alt="" />
+                    </div>
                     <h3 className="team__name">
                         Lorem Ipsum
                     </h3>
                     <p className="team__degree">Mgr fizjoterapii</p>
                 </div>
                 <div className="team__card card">
-                    <img className="team__image" src="img/team/Ewa Bajraszewska.jpg" alt="" />
+                    <div className="team__image team__image--wrapper">
+                        <Image objectFit='contain' className="team__image" fill src="img/team/Ewa Bajraszewska.jpg" alt="" />
+                    </div>
                     <h3 className="team__name">
                         Lorem Ipsum
                     </h3>
                     <p className="team__degree">Lek. med. spec. fizjoterapii i balneoklimatologii</p>
                 </div>
                 <div className="team__card card">
-                    <img className="team__image" src="img/team/Joanna Zakrzewska.jpg" alt="" />
+                    <div className="team__image team__image--wrapper">
+                        <Image objectFit='contain' className="team__image" fill src="img/team/Joanna Zakrzewska.jpg" alt="" />
+                    </div>
                     <h3 className="team__name">
                         Lorem Ipsum
                     </h3>
                     <p className="team__degree">Fizjoterapeuta</p>
                 </div>
                 <div className="team__card card">
-                    <img className="team__image" src="img/team/Małgorzata Łężna.jpg" alt="" />
+                    <div className="team__image team__image--wrapper">
+                        <Image objectFit='contain' className="team__image" fill src="img/team/Małgorzata Łężna.jpg" alt="" />
+                    </div>
                     <h3 className="team__name">
                         Lorem Ipsum
                     </h3>
                     <p className="team__degree">Fizjoterapeuta</p>
                 </div>
                 <div className="team__card card">
-                    <img className="team__image" src="img/team/Marek Stasiak.jpg" alt="" />
+                    <div className="team__image team__image--wrapper">
+                        <Image objectFit='contain' className="team__image" fill src="img/team/Marek Stasiak.jpg" alt="" />
+                    </div>
                     <h3 className="team__name">
                         Lorem Ipsum
                     </h3>
                     <p className="team__degree">Mgr fizjoterapii</p>
                 </div>
                 <div className="team__card card">
-                    <img className="team__image" src="img/team/Mariola Bejnarowicz.jpg" alt="" />
+                    <div className="team__image team__image--wrapper">
+                        <Image objectFit='contain' className="team__image" fill src="img/team/Mariola Bejnarowicz.jpg" alt="" />
+                    </div>
                     <h3 className="team__name">
                         Lorem Ipsum
                     </h3>
                     <p className="team__degree">Lek. med. spec. fizjoterapii i balneoklimatologii</p>
                 </div>
                 <div className="team__card card">
-                    <img className="team__image" src="img/team/Mariola Celmer.jpg" alt="" />
+                    <div className="team__image team__image--wrapper">
+                        <Image objectFit='contain' className="team__image" fill src="img/team/Mariola Celmer.jpg" alt="" />
+                    </div>
                     <h3 className="team__name">
                         Lorem Ipsum
                     </h3>
                     <p className="team__degree">Fizjoterapeuta</p>
                 </div>
                 <div className="team__card card">
-                    <img className="team__image" src="img/team/Patrycja Kawałek.jpg" alt="" />
+                    <div className="team__image team__image--wrapper">
+                        <Image objectFit='contain' className="team__image" fill src="img/team/Patrycja Kawałek.jpg" alt="" />
+                    </div>
                     <h3 className="team__name">
                         Lorem Ipsum
                     </h3>
                     <p className="team__degree">Mgr fizjoterapii</p>
                 </div>
                 <div className="team__card card">
-                    <img className="team__image" src="img/team/empty.jpg" alt="" />
+                    <div className="team__image team__image--wrapper">
+                        <Image objectFit='contain' className="team__image" fill src="img/team/empty.jpg" alt="" />
+                    </div>
                     <h3 className="team__name">
                     Karolina Świderska-Karlikowska
                     </h3>
                     <p className="team__degree">Mgr fizjoterapii</p>
                 </div>
                 <div className="team__card card">
-                    <img className="team__image" src="img/team/empty.jpg" alt="" />
+                    <div className="team__image team__image--wrapper">
+                        <Image objectFit='contain' className="team__image" fill src="img/team/empty.jpg" alt="" />
+                    </div>
                     <h3 className="team__name">
                         Danuta Kamińska 
                     </h3>
